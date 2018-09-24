434c9940149558b30ea5e5ab7da185c49afc7c19
Rechkalov Aleksandr <rechkalov.ae@skbkontur.ru>
2018-09-24 13:09:02 +0500

    lesson work

--- tasks/1.tic-tac-toe/index.js
+++ tasks/1.tic-tac-toe/index.js
@@ -3,15 +3,23 @@ const ZERO = 'O';
 const EMPTY = ' ';
 
 const container = document.getElementById('fieldWrapper');
+let map;
+let isCrossCurrentPlayer = true;
+let isGameOver = false;
+let dimension = 10;
+let countToWin = 5;
 
 startGame();
 addResetListener();
 
-function startGame () {
-    renderGrid(3);
+function startGame() {
+    map = createEmptyMap(dimension);
+    isCrossCurrentPlayer = true;
+    isGameOver = false;
+    renderGrid(dimension);
 }
 
-function renderGrid (dimension) {
+function renderGrid(dimension) {
     container.innerHTML = '';
 
     for (let i = 0; i < dimension; i++) {
@@ -26,41 +34,126 @@ function renderGrid (dimension) {
     }
 }
 
-function cellClickHandler (row, col) {
-    // Пиши код тут
-    console.log(`Clicked on cell: ${row}, ${col}`);
-
-
-    /* Пользоваться методом для размещения символа в клетке так:
-        renderSymbolInCell(ZERO, row, col);
-     */
+function createEmptyMap(dimension) {
+    let map = [];
+    for (let row = 0; row < dimension; row++) {
+        map[row] = [];
+        for (let col = 0; col < dimension; col++) {
+            map[row][col] = undefined;
+        }
+    }
+    return map;
 }
 
-function renderSymbolInCell (symbol, row, col, color = '#333') {
+function cellClickHandler(row, col) {
+    if (isGameOver) {
+        return;
+    }
+    const currentSymbol = isCrossCurrentPlayer ? CROSS : ZERO;
+    if (map[row][col] === undefined) {
+        renderSymbolInCell(currentSymbol, row, col);
+        map[row][col] = currentSymbol;
+        isCrossCurrentPlayer = !isCrossCurrentPlayer;
+        let winnerLine = checkWinner(map, row, col);
+        if (winnerLine.length !== 0) {
+            for (let item of winnerLine) {
+                renderSymbolInCell(currentSymbol, item.row, item.col, "#ff0000")
+            }
+            alert(currentSymbol);
+            isGameOver = true;
+        }
+        if (checkDraw(map)) {
+            alert("Победила дружба");
+            isGameOver = true;
+        }
+    }
+}
+
+function checkWinner(map, row, col) {
+    const currentSymbol = map[row][col];
+    for (let item of getLineData(countToWin, row, col)) {
+        let currentLine = checkLineOnWinner(getLineOfSymbols(map, item.start, item.delta, item.count), currentSymbol, countToWin);
+        if (currentLine.length != 0) {
+            return currentLine;
+        }
+    }
+    return [];
+}
+
+function getLineData(lineSize, row, col) {
+    return [
+        {start: {x: col - lineSize + 1, y: row - lineSize + 1}, delta: {x: 1, y: 1}, count: lineSize * 2 - 1},
+        {start: {x: col, y: row - lineSize + 1}, delta: {x: 0, y: 1}, count: lineSize * 2 - 1},
+        {start: {x: col - lineSize + 1, y: row}, delta: {x: 1, y: 0}, count: lineSize * 2 - 1},
+        {start: {x: col + lineSize - 1, y: row + lineSize - 1}, delta: {x: -1, y: -1}, count: lineSize * 2 - 1}
+
+    ];
+}
+
+function getLineOfSymbols(map, start, delta, count) {
+    let result = [];
+    for (let index = 0; index < count; index++) {
+        let rowIndex = start.y + delta.y * index;
+        let colIndex = start.x + delta.x * index;
+        let row = map[rowIndex];
+        let value = row === undefined ? row : row[colIndex];
+        result.push({row: rowIndex, col: colIndex, value: value});
+    }
+    return result;
+}
+
+function checkLineOnWinner(line, symbol, countToWin) {
+    let result = [];
+    for (let item of line) {
+        if (item.value === symbol) {
+            result.push(item);
+        } else {
+            result = [];
+        }
+        if (result.length === countToWin) {
+            return result;
+        }
+    }
+    return [];
+}
+
+function checkDraw(map) {
+    for (let row of map) {
+        for (let col of row) {
+            if (col === undefined) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function renderSymbolInCell(symbol, row, col, color = '#333') {
     const targetCell = findCell(row, col);
 
     targetCell.textContent = symbol;
     targetCell.style.color = color;
 }
 
-function findCell (row, col) {
+function findCell(row, col) {
     const targetRow = container.querySelectorAll('tr')[row];
     return targetRow.querySelectorAll('td')[col];
 }
 
-function addResetListener () {
+function addResetListener() {
     const resetButton = document.getElementById('reset');
     resetButton.addEventListener('click', resetClickHandler);
 }
 
-function resetClickHandler () {
-    console.log('reset!');
+function resetClickHandler() {
+    startGame();
 }
 
 
 /* Test Function */
+
 /* Победа первого игрока */
-function testWin () {
+function testWin() {
     clickOnCell(0, 2);
     clickOnCell(0, 0);
     clickOnCell(2, 0);
@@ -71,7 +164,7 @@ function testWin () {
 }
 
 /* Ничья */
-function testDraw () {
+function testDraw() {
     clickOnCell(2, 0);
     clickOnCell(1, 0);
     clickOnCell(1, 1);
@@ -84,6 +177,6 @@ function testDraw () {
     clickOnCell(2, 2);
 }
 
-function clickOnCell (row, col) {
+function clickOnCell(row, col) {
     findCell(row, col).click();
 }
